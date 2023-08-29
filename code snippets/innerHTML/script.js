8376061ae209812b956c4686ff5c619374121936
Joy097 <shihabrumeechowdhury@gmail.com>
2023-08-29 20:07:15 +0600

    update 21

--- code snippets/innerHTML/script.js
+++ code snippets/innerHTML/script.js
@@ -1,4 +1,4 @@
 const fruits = ['banana', 'mango', 'tomato', 'orange']
 var length = fruits.length
-for (var i = 0; i <length; i++) {
-    document.getElementById("fruits").innerHTML = fruits[i]}
+
+    document.getElementById("fruits").innerHTML = fruits
