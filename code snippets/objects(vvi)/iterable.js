f76e237919b0a91e30e5c32ca867a5a3aa245912
Joy097 <shihabrumeechowdhury@gmail.com>
2023-09-12 21:03:55 +0600

    update 58

--- code snippets/objects(vvi)/iterable.js
+++ code snippets/objects(vvi)/iterable.js
@@ -1,7 +1,18 @@
 // js can iterate array cause there is a symbol.iterator in array
 // but can not iterate through object because object do not have them
 
-const nums = [1,2,3,4,5,6,7,8,9];
+const nums = [1,2,3];
 const iterator = nums[Symbol.iterator]();
 
-for(let n of nums)
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+
+//make an object iterable:
+const numbers = {
+    one:1,
+    two:2,
+    three:3
+}
+
