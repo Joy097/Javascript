64a6094b1da99c981a3242063120c25996daabdd
Joy097 <shihabrumeechowdhury@gmail.com>
2023-09-13 17:38:21 +0600

    update 3

--- code snippets/functions/arrow_funct.js
+++ code snippets/functions/arrow_funct.js
@@ -1,6 +1,9 @@
 const z = function (x,y){return x*y};
+
 const n = (x,y) => x*y;
+
 const v = (x,y) => {
     x=4*y;
     return x*y;};
+
 const c = () => 5;
--- code snippets/functions/closure(vi).js
+++ code snippets/functions/closure(vi).js
@@ -1,2 +1,15 @@
 // closure is about global and local variable
 
+let counter = 0; //global variable
+
+let result = () => {
+    let counter = 0;
+    let a = 7;   //local variable
+    counter += a;  
+    return counter 
+}
+
+let e = result();
+
+console.log(e);
+console.log(counter)
