e4dd2d642b4b20afbd2c7d12145b952edaf54643
Joy097 <shihabrumeechowdhury@gmail.com>
2023-09-13 12:06:20 +0600

    update 9

--- code snippets/functions/window and this.js
+++ code snippets/functions/window and this.js
@@ -1,1 +1,14 @@
-// window and this 
+// window is the whole window and a function is under that
+// this means the object the function is inside of
+
+function calc(){
+    console.log(this)
+}
+calc(); //returns window
+
+const x = {
+    a:function(){
+    console.log(this)
+}}
+x.a(); //returns x
+
