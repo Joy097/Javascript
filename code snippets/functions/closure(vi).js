bcf33e07c076fab228182efd3a67a30c76d730ee
Joy097 <shihabrumeechowdhury@gmail.com>
2023-09-13 18:08:32 +0600

    update 6

--- code snippets/functions/closure(vi).js
+++ code snippets/functions/closure(vi).js
@@ -7,7 +7,16 @@ function temporary() {
         counter += 1;
     }
   }
+  
 
 const add = temporary(); //so, now add is a function
 
+console.dir(add) //Closure is counter = 2, as add() is called twice
 add();
+add();
+
+// So, closure is a function, having access to the parent scope
+// evem after the parent function has closed. Here, the
+// counter in temporary is kept in a location to use later
+// and in this way, user can access a variable of that scope
+// outside the scope using closure.
