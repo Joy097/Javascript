560fbfa82c70a4d57ec96eafd644c27d18026c0f
Joy097 <shihabrumeechowdhury@gmail.com>
2023-09-13 18:58:51 +0600

    update 11

--- code snippets/class/basics.js
+++ code snippets/class/basics.js
@@ -1,1 +1,14 @@
-class Cas
+class Car{
+    constructor(name, year){
+        this.name = name;
+        this.year = year;
+    }
+
+    race(){     // this is method not function
+        console.log(this.name+' is racing');
+    }
+}
+
+const car1 = new Car('BMW',2007);
+console.log(car1.name);
+car1.race();
