2c8ec0247fac0c07e6722e5a8999054421a2fd57
Joy097 <shihabrumeechowdhury@gmail.com>
2023-09-13 20:59:39 +0600

    update 23

--- code snippets/Async/async.js
+++ code snippets/Async/async.js
@@ -2,10 +2,16 @@
 // functions are synchronous. This is used to skip a function
 // and execute it later.
 
+// setInterval is same but prints unlimited values
+
 console.log("line 1");
 
 setTimeout(function() {
     console.log("line 2");
 },100);
 
-console.log("line 3");
+setInterval(function() {
+    console.log("line 3");
+},100);
+
+console.log("line 4");
