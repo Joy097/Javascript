bf3a936a08f515ab2186598826ecdc0d207d0e06
Joy097 <shihabrumeechowdhury@gmail.com>
2023-09-13 19:59:14 +0600

    update 17

--- code snippets/class/static method.js
+++ code snippets/class/static method.js
@@ -2,3 +2,14 @@
 // so, static is used when we need to use a function
 // without calling the through an object
 
+class Car {
+    constructor(name) {
+      this.name = name;
+    }
+    static hello(x) {
+      return "Hello " + x.name;
+    }
+  }
+const myCar = new Car("Ford");
+console.log(Car.hello(myCar));
+  
