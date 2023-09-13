97078cb316d1f60b47cc20da52b1f5cae8959e80
Joy097 <shihabrumeechowdhury@gmail.com>
2023-09-14 00:52:23 +0600

    update 3

--- code snippets/Async/callback_async_example.js
+++ code snippets/Async/callback_async_example.js
@@ -1,8 +1,27 @@
 const give_cv = true;
 const mark = 80;
 
-function written_exam(){
-    if (give_cv) setTimeout(
-        
-    )
+function written_exam(callback) {
+    console.log('CV review in progress');
+    setTimeout(function(){
+        if(give_cv) callback()
+
+    },2000)
+
 }
+
+function viva(callback) {
+    console.log('mark review in progress');
+    if (mark>=80) setTimeout(
+        callback(),1000)
+
+    else console.log('The mark is not satisfactory')
+}
+
+function Job() {
+    console.log('You got the job!!')
+}
+
+written_exam(function(){
+    viva(Job)
+})
