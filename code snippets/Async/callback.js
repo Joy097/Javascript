ee4b6b5316a0a2b20e84459775082927c1b06275
Joy097 <shihabrumeechowdhury@gmail.com>
2023-09-13 20:19:22 +0600

    update 19

--- code snippets/Async/callback.js
+++ code snippets/Async/callback.js
@@ -1,10 +1,27 @@
+// callback is a function that can be passed as a 
+// parameter inside another function.
+
 function display(value){
     console.log(value);
 }
 
-function calculate(num1, num2){
+function calculate(num1, num2, callback){
     let sum = num1 + num2
-    display(sum)
+    callback(sum)
 }
 
-let result = calculate(3,8)
+calculate(3, 8, display)
+
+// here display function is passed as a parameter
+// So, we can control whether we want to use display or not
+
+// In other words:
+
+function calculate(num1, num2, callback){
+    let sum = num1 + num2
+    callback(sum)
+}
+
+calculate(3, 8, function(value){
+    console.log(value)
+})
