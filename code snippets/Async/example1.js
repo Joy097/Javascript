49fb4ba452c0a74ae5a0a21e1f824adb224a5ed3
Joy097 <shihabrumeechowdhury@gmail.com>
2023-09-14 12:55:05 +0600

    update 2

--- code snippets/Async/example1.js
+++ code snippets/Async/example1.js
@@ -12,23 +12,27 @@ function enroll(callback){
         else {
             console.log("Please clear the payment!")
         }
-    })
+    },2000)
 }
 
 function progress(callback){
     console.log("Conducting the course...")
     setTimeout(function(){
         if (marks>=80) {
-            console.log("Conductii")
+            console.log("Completed the course!")
             callback();
         }
 
         else {
             console.log("Please clear the payment!")
         }
-    })
+    },3000)
 }
 
-function getCertificate(callback){
+function getCertificate(){
     console.log("Congrats! here is your certificate")
 }
+
+enroll(function(){
+    progress();
+})
