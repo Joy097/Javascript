fe1f2464adceff4a67a39b7a1980a0cc370cf202
Joy097 <shihabrumeechowdhury@gmail.com>
2023-09-16 20:01:44 +0600

    update 9

--- code snippets/Async Await/example1 to promise.js
+++ code snippets/Async Await/example1 to promise.js
@@ -5,22 +5,29 @@ function enroll(callback){
     console.log('Enrolling');
     const promise = new Promise(function(resolve, reject){
         setTimeout(function(){
-            if(payment) callback();
-            else console.log('Payment not cleared');
+            if(payment) resolve();
+            else reject('Payment not cleared');
         },2000)
     })
+
+    return promise
 }
 
 function progress(callback){
     console.log('Course in progress');
-    setTimeout(function(){
-        if(marks>=80) callback();
-        else console.log('Your mark is poor');
-    },3000)
+    const promise = new Promise(function(resolve, reject){
+        setTimeout(function(){
+            if(marks>=80) resolve();
+            else reject('Your mark is poor');
+        },3000)
+    
+    })
+
+    return promise
 }
 
 function getCertificate(){
-    console.log('Congrats! Your are getting certificate');
+    const promise = new Promise(function)
 }
 
 enroll(function(){
