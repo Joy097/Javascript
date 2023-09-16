665af8e42f1d969721ea196d5b0eebdd95330df8
Joy097 <shihabrumeechowdhury@gmail.com>
2023-09-16 19:21:29 +0600

    update 5

--- code snippets/Async/promise.js
+++ code snippets/Async/promise.js
@@ -1,9 +1,23 @@
 console.log("task 1")
 
-const promise = new Promise()
+const execute = false;
+const promise = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        if(execute) {
+            resolve("task 2")
+        }
+        else{
+            reject("can't execute task")
+        }
+    },3000)
+})
 
 promise
-    .then()
-    .catch()
+    .then(function(value){
+        console.log(value)
+    })
+    .catch(function(err){
+        console.log(err)
+    })
 
 console.log("task 3")
