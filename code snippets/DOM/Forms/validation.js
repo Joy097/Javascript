8f7802442a03e5717cde9df27db40bd4355737dc
Joy097 <shihabrumeechowdhury@gmail.com>
2023-09-21 09:48:18 +0600

    update 7

--- code snippets/DOM/Forms/validation.js
+++ code snippets/DOM/Forms/validation.js
@@ -1,3 +1,7 @@
 const form = document.forms['myform'];
 name = form['fname'].value;
-name = form['fname'].value;
+id = form['fid'].value;
+
+if (name === '' || id === ''){
+    alert('Please enter value')
+}
