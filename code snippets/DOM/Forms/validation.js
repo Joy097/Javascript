e9e8af9e2ac1c8b8a68bcbc0afb1803071f0dd52
Joy097 <shihabrumeechowdhury@gmail.com>
2023-09-21 10:02:43 +0600

    update 14

--- code snippets/DOM/Forms/validation.js
+++ code snippets/DOM/Forms/validation.js
@@ -3,8 +3,8 @@ name = form['fname'].value;
 id = form['fid'].value;
 
 if (name === '' || id === ''){
-    alert('Please enter value')
+    alert('Please enter value');
 }
-elif (id <=0 || id >=10){
-    alert('Please enter a valid ')
+else if (id <=0 || id >=10){
+    alert('Please enter a valid id');
 }
