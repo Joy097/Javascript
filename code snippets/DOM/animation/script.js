77b653501eed945c08ad580232164fa2cef07f04
Joy097 <shihabrumeechowdhury@gmail.com>
2023-09-25 23:32:29 +0600

    update 9

--- code snippets/DOM/animation/script.js
+++ code snippets/DOM/animation/script.js
@@ -1,4 +1,16 @@
 function myMove(){
     const animate = document.getElementById('animate');
-    
+    let pos = 0
+
+    setInterval(frame,5)
+    function frame(){
+        if(pos<350){
+            pos++;
+            animate.style.top = pos + 'px';
+            animate.style.left = pos + 'px';
+        }
+        else{
+            
+        }
+    }
 }
