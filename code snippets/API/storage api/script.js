ed580f039ec416200563ea801da6ee7ae5e0236a
Joy097 <shihabrumeechowdhury@gmail.com>
2023-10-02 22:09:55 +0600

    update 28

---
+++ code snippets/API/storage api/script.js
@@ -0,0 +1,15 @@
+function setLocalStorage(key, value) {
+    localStorage.setItem(key, value);
+}
+
+function getLocalStorage(key) {
+    localStorage.getItem(key);
+}
+
+function remLocalStorage(key) {
+    localStorage.removeItem(key);
+}
+
+function clrLocalStorage(key) {
+    localStorage.clearItem(key);
+}
