b90640a1eeafd01cd7bec903012694f5ed768ec3
Joy097 <shihabrumeechowdhury@gmail.com>
2023-10-02 21:24:21 +0600

    update 19

--- code snippets/API/Form Api/script.js
+++ code snippets/API/Form Api/script.js
@@ -1,5 +1,14 @@
 function validation(){
     const inpObj = document.getElementById("id1")
+
+    if(!inpObj.checkValidity()){
+        document.getElementById("demo").innerHTML = inpObj.validationMessage;
+    }
+
+    if(!inpObj.checkValidity()){
+        document.getElementById("demo").innerHTML = inpObj.validationMessage;
+    }
+
     if(!inpObj.checkValidity()){
         document.getElementById("demo").innerHTML = inpObj.validationMessage;
     }
