9c7414fc1e9670fee1d37a0bc1ddb8d5ac6ab87e
Joy097 <shihabrumeechowdhury@gmail.com>
2023-10-03 12:50:53 +0600

    update 22

--- code snippets/AJAX/intro/script.js
+++ code snippets/AJAX/intro/script.js
@@ -1,4 +1,8 @@
 function loadData(){
     const xhttpr = new XMLHttpRequest();
-    
+
+    xhttpr.onload = function(){
+        const container = document.getElementById('demo')
+        demo.
+    }
 }
