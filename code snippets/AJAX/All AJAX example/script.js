50e0ec34be21290dcfd4bd368295db2b2f10e33e
Joy097 <shihabrumeechowdhury@gmail.com>
2023-10-03 16:57:02 +0600

    update 8

--- code snippets/AJAX/All AJAX example/script.js
+++ code snippets/AJAX/All AJAX example/script.js
@@ -1,21 +1,24 @@
 function sendRequest(method, url){
+
+    const promise = new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
 
     xhr.onload = function(){
-        console.log(this.responseText);
+        resolve(this.response);
     }
 
     xhr.open(method,url)
-
     xhr.responseType = 'json';
-
     xhr.send();
 
+    })   
 }
 
 function getData(){
     sendRequest('GET','https://jsonplaceholder.typicode.com/todos/1')
+    .then(responseData => console.log(responseData));
 }
+
 function sendData(){}
 
 const getBtn = document.getElementById('get');
