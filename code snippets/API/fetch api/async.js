8dd9bd664915f581c713ee944066dbec50711181
Joy097 <shihabrumeechowdhury@gmail.com>
2023-10-03 12:10:38 +0600

    update 18

---
+++ code snippets/API/fetch api/async.js
@@ -0,0 +1,7 @@
+const display = document.getElementById('demo');
+
+async function getData(){
+    const res = await fetch("http://127.0.0.1:5500/API/fetch%20api/data.txt")
+    const data = res.text();
+    display.innerHTML = data
+}
--- code snippets/API/fetch api/promise.js
+++ code snippets/API/fetch api/promise.js
@@ -1,6 +1,11 @@
 const display = document.getElementById('demo');
 
 function getData(){
-    fetch("https://www.lipsum.com/");
-        .then(res=>console.log(res));
+    fetch("http://127.0.0.1:5500/API/fetch%20api/data.txt")
+    .then(res => res.text())
+    .then(data => {
+        console.log("async pro2")
+        display.innerHTML = data
+    });
+    console.log("async pro1")
 }
