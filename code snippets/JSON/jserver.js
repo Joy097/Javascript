e8ceff98f2b99d5bcdaa81dc7a78dbe29f2522cd
Joy097 <shihabrumeechowdhury@gmail.com>
2023-10-04 23:33:47 +0600

    update 12

--- code snippets/JSON/jserver.js
+++ code snippets/JSON/jserver.js
@@ -1,4 +1,8 @@
+//how to fetch data from server
 async function fetchData(){
-    const response = await fetch("demo.txt")
-    const data = await response.json
+    const response = await fetch("json.txt");
+    const data = await response.json();
+    console.log(data);
 }
+
+fetchData()
