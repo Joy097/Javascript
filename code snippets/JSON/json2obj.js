c7ecc106f022f6df3906f413495451949df48fee
Joy097 <shihabrumeechowdhury@gmail.com>
2023-10-04 20:57:08 +0600

    update 4

--- code snippets/JSON/json2obj.js
+++ code snippets/JSON/json2obj.js
@@ -1,1 +1,7 @@
-const jsonString = '{"name":"John","age":30,}'
+const jsonString = '{"name":"John","age":30,"city":"Dhaka"}'
+
+console.log(jsonString) // unusable
+
+const myObj = JSON.parse(jsonString)
+
+console.log(myObj)  // unusable 
